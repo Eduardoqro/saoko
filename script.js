bd65bea62af172caa64e08dbdd8f9c40e8b8539c
Eduardoqro <eduardomendozaqro@gmail.com>
2022-09-26 10:52:28 -0500

    feat:uso de script exteno

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Saoko papi saoko") 
+        document.getElementById("generation").style.color="grey";  
+        function colorTitulo(){
+            console.log("Pulso el boton de Generation")
+            document.getElementById("generation").style.color="blue"
+        }
