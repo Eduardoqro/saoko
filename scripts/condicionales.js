6a56941898d42e839a814519e79420d6d34f3986
Eduardoqro <eduardomendozaqro@gmail.com>
2022-09-27 17:18:34 -0500

    feat:condicionales ejercicios

--- scripts/condicionales.js
+++ scripts/condicionales.js
@@ -111,3 +111,110 @@ edad=25;
         comparacion="*No se puede saber*";
  }
  console.log(`la edad ${comparacion} a 25`);
+
+
+ console.log("--------------Ejercicio-----------------");
+ let numeroMes=6;
+ let estacion = "", mes = "";
+
+switch (numeroMes) {
+  case 1:
+    mes = "Enero";
+    estacion = "Invierno";
+    break;
+  case 2:
+    mes = "Febrero";
+    estacion = "Invierno";
+    break;
+  case 3:
+    mes = "Marzo";
+    estacion = "Primavera";
+    break;
+  case 4:
+    mes = "Abril";
+    estacion = "Primavera";
+    break;
+  case 5:
+    mes = "Mayo";
+    estacion = "Primavera";
+    break;
+  case 6:
+    mes = "Junio";
+    estacion = "Primavera";
+    break;
+  case 7:
+    mes = "Julio";
+    estacion = "Primavera";
+    break;
+  case 8:
+    mes = "Agosto";
+    estacion = estacion = "Primavera";
+    break;
+  case 9:
+    mes = "Septiembre";
+    estacion = "Otoño";
+    break;
+  case 10:
+    mes = "Octubre";
+    estacion = "Otoño";
+    break;
+  case 11:
+    mes = "Noviembre";
+    estacion = "Otoño";
+    break;
+  case 12:
+    mes = "Diciembre";
+    estacion = "Invierno";
+    break;
+  default:
+        console.log("*no hay mes*");
+}
+console.log(`El mes de ${mes} se encuentra en la estación ${estacion}`);
+
+//************Otro Ejercicio********* */
+
+numeroMes=6;
+ estacion = "", mes = "";
+ if (numeroMes === 12 || numeroMes === 1 || numeroMes === 2) {
+    estacion = "Invierno";
+    if (numeroMes === 12) mes = "Diciembre";
+    else if (numeroMes === 1) mes = "Enero";
+    else mes = "Febrero";
+  }
+  else if (numeroMes >= 3 && numeroMes <= 5) {
+    estacion = "Primavera";
+    if (numeroMes === 3) mes = "Marzo";
+    else if (numeroMes === 4) mes = "Abril";
+    else mes = "Mayo";
+  }
+  else if (numeroMes >= 6 && numeroMes <= 8) {
+    estacion = "Verano";
+    if (numeroMes === 6) mes = "Junio";
+    else if (numeroMes === 7) mes = "Julio";
+    else mes = "Agosto";
+  }
+  else if (numeroMes >= 9 && numeroMes <= 11) {
+    estacion = "Otoño";
+    if (numeroMes === 9) mes = "Septiembre";
+    else if (numeroMes === 10) mes = "Octubre";
+    else mes = "Noviembre";
+  }
+  else {
+    estacion = "No se encontro";
+    mes = "No se encontro";
+  }
+
+  //************Otro Ejercicio********* */
+
+  let online=true;
+  let active=true;
+  if(online){
+    if(active){
+        console.log('James')
+    }
+  }
+
+online && active && console.log('James');  // es el codigo de arriba pero de foma simplificada , es el operador AND de corto circuito.
+
+
+  
